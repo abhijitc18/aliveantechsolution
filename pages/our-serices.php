7c4fbb1c1cde1acddfe3ad682f206a99de6e6d54
abhijitc18 <chouguleabhijit18@gmail.com>
2025-11-14 19:08:12 +0530

    ui: images are added in the assets folder

---
+++ pages/our-serices.php
@@ -0,0 +1,49 @@
+<?php require '../includes/header.php'; ?>
+
+<!-- page title -->
+<section class="banner-section bg-cover overlay" data-background="/aliveantechsolution/assets/images/backgrounds/page-title.jpg">
+    <div class="container">
+        <div class="row">
+            <div class="col-12">
+                <h2 class="text-white mt-3">Our Services</h2>
+                <!-- breadcrumb -->
+                <nav aria-label="breadcrumb">
+                    <ol class="breadcrumb bg-transparent p-0">
+                        <li class="breadcrumb-item font-weight-semebold">
+                            <a class="text-white" href="/aliveantechsolution/index">Home</a>
+                        </li>
+                        <li class="breadcrumb-item font-weight-semebold active text-primary" aria-current="page">Our Services</li>
+                    </ol>
+                </nav>
+            </div>
+        </div>
+    </div>
+</section>
+<!-- /page title -->
+
+<!-- about us -->
+<section class="section position-relative">
+    <div class="container ">
+        <div class="row justify-content-between mb-80">
+            <div class="col-lg-6 col-md-6 mb-8 mb-md-0 ">
+                <span class="section-title-border "></span>
+                <p class="subtitle">Welcome to </p>
+                <h3 class="section-title">Alivean Tech Solutions</h3>
+                <p style="text-align: justify;">
+                    We understand your tech with quality services first time, right time and every time Alivean Tech Solutions is an emerging company which offers a firsthand service in the various and extremes field of information technology. We are the best deal in the market that provides clients with well accomplished services. We consistently meet & exceed expectations of people who seek flawless services in the field of online business.
+                </p>
+                <p style="text-align: justify; ">
+                    At Alivean Tech Solutions, we guarantee complete customer satisfaction. We are customized to fit into business requirement and enhance business goal with a result oriented performance.
+                </p>
+                <a href="/aliveantechsolution/pages/our-serices" class="btn btn-primary hover-ripple">more service</a>
+            </div>
+            <div class="col-lg-6 col-md-6 mb-4 mb-md-0">
+                <img class="img-about" src="/aliveantechsolution/assets/images/about/about-us.webp" alt="Alivean Tech Solutions">
+            </div>
+        </div>
+    </div>
+</section>
+</div>
+</section>
+
+<?php require '../includes/footer.php'; ?>
